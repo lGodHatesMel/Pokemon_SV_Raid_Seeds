666f889b772a09f4ed7c73537e3d7f1f27af9a27
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-05-06 02:05:14 -0400

    Update Decimal-to-HexDecimal.py

--- Convert Scripts/Decimal-to-HexDecimal.py
+++ Convert Scripts/Decimal-to-HexDecimal.py
@@ -4,7 +4,7 @@ import json
 with open("config.json", "r") as input_file:
     input_data = json.load(input_file)
 
-# Function to recursively convert decimal values to hexadecimal values
+# Function to convert decimal values to hexadecimal values
 def convert_dec_to_hex(data):
     if isinstance(data, dict):
         # If the value is a dictionary, recursively convert its values
@@ -14,7 +14,7 @@ def convert_dec_to_hex(data):
         return [convert_dec_to_hex(value) for value in data]
     elif isinstance(data, int):
         # If the value is a decimal integer, convert it to hexadecimal
-        return hex(data)[2:]
+        return hex(data)[2:].upper()
     else:
         # If the value is not a dictionary, list, or decimal integer, return it unchanged
         return data
@@ -24,4 +24,7 @@ output_data = convert_dec_to_hex(input_data)
 
 # Write to a new file
 with open("config.txt", "w") as output_file:
+    json.dump(output_data, output_file, indent=4)
+
+with open('newconfig.json', 'w') as json_file:
     json.dump(output_data, output_file, indent=4)
